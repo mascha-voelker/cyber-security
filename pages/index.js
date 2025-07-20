e78d23b748a886f4c4161ebd28c27dd40ad6a1bb
mascha-voelker <mascha.volker@gmail.com>
2025-07-20 17:05:56 +0200

    index.js update

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <div style={{ width: '100%', height: '100vh', margin: 0, padding: 0 }}>
+      <iframe 
+        src="/story.html" 
+        style={{ 
+          width: '100%', 
+          height: '100%', 
+          border: 'none',
+          margin: 0,
+          padding: 0
+        }}
+        title="Cyber Security Course"
+      />
+    </div>
+  )
 }
