1981a0595e91196f9313fa3c6972809cbaeee157
mascha-voelker <mascha.volker@gmail.com>
2025-07-20 15:15:08 +0200

    storyline.js created

---
+++ pages/storyline.js
@@ -0,0 +1,17 @@
+export default function Storyline() {
+  return (
+    <div style={{ width: '100%', height: '100vh', margin: 0, padding: 0 }}>
+      <iframe 
+        src="/story.html" 
+        style={{ 
+          width: '100%', 
+          height: '100%', 
+          border: 'none',
+          margin: 0,
+          padding: 0
+        }}
+        title="Cyber Security Course"
+      />
+    </div>
+  )
+}
